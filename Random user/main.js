7c8d65645ffb9451b99b7a75cb29c10ea858a628
JuwelShaikh <juwelshaikh04@gmail.com>
2023-10-28 15:48:42 +0600

    img added

--- Random user/main.js
+++ Random user/main.js
@@ -6,18 +6,31 @@ const loadData = user =>{
 
 
 const displayUser = us => {
+    // name
     const nameTag = document.getElementById('name')
     const userName = us.results[0].name.title +' '+us.results[0].name.first + ' ' + us.results[0].name.last;
     nameTag.innerText = userName;
 
+    // gender
     const genderTag = document.getElementById('gender')
     const genderName = us.results[0].gender;
     genderTag.innerText = genderName;
 
+    // location
     const locationTag = document.getElementById('loaction')
     const locationName = us.results[0].location.city; 
     locationTag.innerText = locationName;
-    console.log(us.results[0].location.city);
+
+    // street
+    const streetTag = document.getElementById('street')
+    const streetName = us.results[0].location.street.number + ' ' +us.results[0].location.street.name;
+    streetTag.innerText = streetName; 
+
+    // profilePhoto
+    const photoTag = document.getElementById('profile');
+    const photo = us.results[0].picture.medium;
+    photoTag.innerHTML = photo; 
+    console.log(us.results[0].picture.medium);
 }
 
 
